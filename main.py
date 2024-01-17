f669286dec76bb16ab8ae0c7e4576f2141c1525e
lanetb <lanetb@dukes.jmu.edu>
2024-01-17 15:43:55 -0500

    Drawing grid

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import pygame as pg
 import numpy as np
+from snake import *
 from vars import *
+import sys
+from enum import Enum
+
+np.set_printoptions(threshold=sys.maxsize)
+
+
 
 def main():
     pg.init()
     global SCREEN, GAMEBOARD 
     SCREEN = pg.display.set_mode(SCREEN_SIZE)
-    GAMEBOARD = np.zeros((SCREEN_X // BLOCK_SIZE, SCREEN_Y // BLOCK_SIZE) , dtype=int)
+    GAMEBOARD = np.zeros((GAMEBOARD_X, GAMEBOARD_Y) , dtype=int)
     running = True
     SCREEN.fill(BLACK)
-    while running:
-        draw_grid()
+    snake = Snake(SNAKENUMBER, [(translate_to_grid(SCREEN_X//2), translate_to_grid(SCREEN_Y//2))], (0,255,0), Directions.UP, GAMEBOARD)
+
+    print(GAMEBOARD)
+
+    while running: 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-
+        snake.draw(pg, SCREEN)
+        draw_grid()
         pg.display.update()
 
 def draw_grid():
-    for x in range(0, SCREEN_X, BLOCK_SIZE):
-        for y in range(0, SCREEN_Y, BLOCK_SIZE):
-            rect = pg.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
-            pg.draw.rect(SCREEN, GRAY, rect, 1)
+    for i in range(0, GAMEBOARD_X):
+        for j in range(0, GAMEBOARD_Y):
+            if GAMEBOARD[i][j] == SNAKENUMBER:
+                rect = pg.Rect(translate_to_cords(i), translate_to_cords(j), BLOCK_SIZE, BLOCK_SIZE)
+                pg.draw.rect(SCREEN, (0,255,0), rect, 0)
+                pg.draw.rect(SCREEN, GRAY, rect, 1)
+            else:
+                rect = pg.Rect(translate_to_cords(i), translate_to_cords(j), BLOCK_SIZE, BLOCK_SIZE)
+                pg.draw.rect(SCREEN, BLACK, rect, 0)
+                pg.draw.rect(SCREEN, GRAY, rect, 1)
+
+
+
 
 if __name__ == "__main__":
     main()
--- snake.py
+++ snake.py
@@ -1,1 +1,73 @@
+from enum import Enum
+from vars import *
+import numpy as np
 
+class Directions(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+class Snake:
+    length = None
+    direction = None
+    body = None
+    indicator = None
+    block_size = None
+    color = (0,255,0)
+    bounds = None
+    game_board = np.zeros((GAMEBOARD_X, GAMEBOARD_Y) , dtype=int)
+
+
+    def __init__(self, indicator, body, color, direction, game_board):
+        self.indicator = indicator
+        self.bounds = SCREEN_SIZE
+        self.body = body
+        self.block_size = BLOCK_SIZE
+        color = color
+        game_board = game_board
+        game_board[body[0][0]][body[0][1]] = indicator
+        self.respawn(body, direction)
+
+    def respawn(self, body, direction):
+        self.length = 1
+        self.direction = direction
+        self.body = body
+
+    def draw(self, game, window):
+        for segment in self.body:
+            self.game_board[segment[0]][segment[1]] = self.indicator
+
+    def move(self):
+        curr_head = self.body[-1]
+        if self.direction == Directions.DOWN:
+            next_head = (curr_head[0], curr_head[1] + self.block_size)
+            self.body.append(next_head)
+        elif self.direction == Directions.UP:
+            next_head = (curr_head[0], curr_head[1] - self.block_size)
+            self.body.append(next_head)
+        elif self.direction == Directions.RIGHT:
+            next_head = (curr_head[0] + self.block_size, curr_head[1])
+            self.body.append(next_head)
+        elif self.direction == Directions.LEFT:
+            next_head = (curr_head[0] - self.block_size, curr_head[1])
+            self.body.append(next_head)
+
+        if self.length < len(self.body):
+            self.body.pop(0)
+
+    def steer(self, direction):
+        if self.direction == Directions.DOWN and direction != Directions.UP:
+          self.direction = direction
+        elif self.direction == Directions.UP and direction != Directions.DOWN:
+          self.direction = direction
+        elif self.direction == Directions.LEFT and direction != Directions.RIGHT:
+          self.direction = direction
+        elif self.direction == Directions.RIGHT and direction != Directions.LEFT:
+          self.direction = direction
+
+def translate_to_grid(n):
+    return n // BLOCK_SIZE
+
+def translate_to_cords(n):
+    return n * BLOCK_SIZE
